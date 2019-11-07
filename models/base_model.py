c00ab4b6cc099e66529136f175a879715c5b1531
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-11-07 01:48:43 +0000

    updated backup

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+asdsada
+"""
+
+import uuid
+import datetime
+
+class BaseModel:
+
+    def __init__(self, *args, **kwargs):
+        if kwargs is None:
+            for key, value in kwargs.items():
+                if key == 'name':
+                    setattr(self, key, value)
+                if key == 'my_number':
+                    self.my_number = value
+                if key == 'id':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        string = "[{}] ({}) <{}>".format(self.__class__.__name__,
+                                         self.id,
+                                         str(self.__dict__))
+        return string
+
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        my_dict = {'my_number': self.my_number,
+                   'name': self.name,
+                   '__class__': self.__class__.__name__,
+                   'updated_at:': self.updated_at,
+                   'id': self.id,
+                   'created_at': self.created_at}
+        return my_dict
