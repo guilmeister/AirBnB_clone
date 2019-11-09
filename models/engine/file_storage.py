d39045b791bf1595df89176c48b8938739f20dba
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-11-09 00:48:32 +0000

    file storage now working with number 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,7 @@ huehuehue
 """
 
 import json
+from models.base_model import BaseModel
 
 class FileStorage:
 
@@ -13,14 +14,23 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary."""
-        return __objects
+        return self.__objects
 
     def new(self, obj):
         """sets the objects with key"""
-        self.__objects = obj.__class__.__name__.id
+        key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        return json.dumps(__file_path)
+        with open(FileStorage.__file_path, 'w') as g:
+            copy_dict = {key: self.__objects[key].to_dict()
+                        for key in self.__objects}
+            json.dump(copy_dict, g)
 
     def reload(self):
-        return json.loads(__file_path)
+        try:
+            with open(self.__file_path, 'r') as f:
+                for key, value in json.load(f).items():
+                    self.__objects[key] = eval(key.split('.')[0])(**value)
+        except FileNotFoundError:
+            pass
