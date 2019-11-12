d96395a8d2e703c167c0d1fc050cf8ad89190b06
akeemseymens <812@holbertonschool.com>
2019-11-12 14:54:34 -0800

    added setup teard class

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,61 +14,77 @@ class TestBaseClass(unittest.TestCase):
     """
     Test Base Model class unittesting
     """
+    @classmethod
+    def setUpClass(cls):
+        print('setupClass')
 
-    def instance_creation(self):
-        """
-        Create instance of Class Base Model name, number & class type.
-        """
-        dummy = BaseModel()
-        self.assertTrue(type(dummy), BaseModel) 
-        dummy.name = "Holberton"
-        self.assertEqual(dummy, "Holberton")
-        self.assertTrue(type(dummy.name), str)
+    @classmethod
+    def tearDownClass(cls):
+        print('teardownClass')
+        return super().tearDownClass()
+
+    def setUp(self):
+        print('setup')
+        self.self.dummy = BaseModel()
+        self.self.dummy1 = BaseModel()
+        self.self.dummy2 = BaseModel()
+    
+
+    def tearDown(self):
+        print('teardown')
+        return super().tearDown()
+
+        def instance_creation(self):
+        """ Create instance of Class Base Model name, number & class type."""
+
+        self.assertTrue(type(self.dummy), BaseModel) 
+        self.dummy.name = "Holberton"
+        self.assertEqual(self.dummy, "Holberton")
+        self.assertTrue(type(self.dummy.name), str)
         self.my_number = 89
-        self.assertEqual(dummy.my_number, 89)
-        self.assertEqua(type(dummy.my_number),int)
+        self.assertEqual(self.dummy.my_number, 89)
+        self.assertEqua(type(self.dummy.my_number),int)
 
     def instance_creation2(self):
         """ Checking id, created and updated  *_at"""
-        dummy = BaseModel()
-        self.assertEqual(type(dummy.id), str)
-        self.assertEqual(type(dummy.created_at), datetime)
-        self.assertEqual(type(dummy.updated_at), datetime)
+
+        self.assertEqual(type(self.dummy.id), str)
+        self.assertEqual(type(self.dummy.created_at), datetime)
+        self.assertEqual(type(self.dummy.updated_at), datetime)
         
     def default_attributes(self):
         """ id, created_at, updated_at default attributes """
-        dummy = BaseModel()
-        self.assertTrue(hasattr(dummy, "id"))
-        self.assertTrue(hasattr(dummy, "created_at"))
-        self.assertTrue(hasattr(dummy, "updated_at"))
+
+        self.assertTrue(hasattr(self.dummy, "id"))
+        self.assertTrue(hasattr(self.dummy, "created_at"))
+        self.assertTrue(hasattr(self.dummy, "updated_at"))
 
 def test_different_id(self):
         """ id should be different """
-        dummy1 = BaseModel()
-        dummy2 = BaseModel()
-        self.assertNotEqual(dummy1.id, dummy2.id)
-        self.assertEqual(len(dummy1.id), len(dummy2.id))
+        
+        self.assertNotEqual(self.dummy1.id, self.dummy2.id)
+        self.assertEqual(len(self.dummy1.id), len(self.dummy2.id))
 
 def test_strMethod(self):
         """ check __str__ output """
-        dummy = BaseModel()
-        my_str = "[BaseModel] ({}) {}".format(dummy.id, dummy.__dict__)
-        self.assertEqual(my_str, str(dummy))
+
+        my_str = "[BaseModel] ({}) {}".format(self.dummy.id, self.dummy.__dict__)
+        self.assertEqual(my_str, str(self.dummy))
 
 def test_save(self):
         """ save updated_at """
-        dummy = BaseModel()
-        dum1 = dummy.updated_at
-        dummy.save()
-        dum2 = dummy.updated_at
+
+        dum1 = self.dummy.updated_at
+        self.dummy.save()
+        dum2 = self.dummy.updated_at
         self.assertNotEqual(dum1, dum2)
 
 def test_to_dict(self):
         """ dictionary conversion """
-        dummy = BaseModel()
-        dummy.name = "Akeem"
-        dummy.my_number = 89
-        dicti = dummy.to_dict()
+
+        self.dummy.name = "Akeem"
+        self.dummy.my_number = 89
+        dicti = self.dummy.to_dict()
         my_keys = ["id",
                    "name",
                    "my_number",
@@ -82,11 +98,11 @@ def test_to_dict(self):
 
 def test_to_dict_attr(self):
         """ created_at, updated_at values """
-        dummy = BaseModel()
+
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        dicti = dummy.to_dict()
-        self.assertEqual(dicti["created_at"], dummy.created_at.strftime(time_format))
-        self.assertEqual(dicti["updated_at"], dummy.updated_at.strftime(time_format))
+        dicti = self.dummy.to_dict()
+        self.assertEqual(dicti["created_at"], self.dummy.created_at.strftime(time_format))
+        self.assertEqual(dicti["updated_at"], self.dummy.updated_at.strftime(time_format))
         self.assertEqual(dicti["__class__"], "BaseModel")
         self.assertEqual(type(dicti["created_at"]), str)
         self.assertEqual(type(dicti["updated_at"]), str)
