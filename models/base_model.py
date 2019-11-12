6309073fadb20390ec2617b4b9b40f97c1081d65
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-11-12 04:43:50 +0000

    pep8 compliant and added docstrings

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-This is the Baseline Class
+This is a module for Base Class
 
 """
 
@@ -9,13 +9,21 @@ import uuid
 import datetime
 import models
 
-class BaseModel:
 
+class BaseModel:
+    """
+    Class BaseModel
+    """
     def __init__(self, *args, **kwargs):
+        """
+        Function that initializes attributes
+        """
+        formatting = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    setattr(self, key,
+                            datetime.datetime.strptime(value, formatting))
                 elif key == "__class__":
                     continue
                 else:
@@ -27,16 +35,28 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
+        """
+        Return string representation of an object
+        """
         string = "[{}] ({}) {}".format(self.__class__.__name__,
-                                         self.id,
-                                         self.__dict__)
+                                       self.id,
+                                       self.__dict__)
         return string
 
     def save(self):
+        """
+        Function that updates the public instance attribute
+        updated_at with current datetime
+        """
         self.updated_at = datetime.datetime.now()
         models.storage.save()
 
     def to_dict(self):
+        """
+        Function that returns a dictionary with the class name
+        of the object and created_at and updated_at attributes
+        are converted to string format in ISO format
+        """
         my_dict = self.__dict__.copy()
         my_dict.update({'__class__': str(type(self).__name__)})
         my_dict["updated_at"] = self.updated_at.isoformat()
