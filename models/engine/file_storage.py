c454cc5a236c7dcc1076404a603209f5be2dc2df
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-11-12 07:47:54 +0000

    added docstrings, added working imports, pep8 compliant

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,9 +6,18 @@ This is a module for FileStorage Class
 
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
+    """
+    Class FileStorage
+    """
 
     __file_path = "file.json"
     __objects = {}
