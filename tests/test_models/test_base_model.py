62de579ef06c0fdb092bcaa97a2683b1c358c37b
akeemseymens <812@holbertonschool.com>
2019-11-12 13:55:17 -0800

    added some tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,7 +3,10 @@
 """
 Testing outputs for BaseModel
 """
-
+import unittest
+from datetime import datetime, date, time
+import uuid
+from models import storage
 from models.base_model import BaseModel
 
 
@@ -14,6 +17,21 @@ class TestBaseClass(unittest.TestCase):
 
     def instance_creation(self):
         """
-        Create instance of Class Base Model
+        Create instance of Class Base Model name, number & class type.
         """
-        self.base = BaseModel()
+        creation_inst = BaseModel()
+        self.assertTrue(type(creation_inst), BaseModel) 
+        creation_inst.name = "Holberton"
+        self.assertEqual(creation_inst, "Holberton")
+        self.assertTrue(type(creation_inst.name), str)
+        self.my_number = 89
+        self.assertEqual(creation_inst.my_number, 89)
+        self.assertEqua(type(creation_inst.my_number),int)
+
+    def instance_creation2(self):
+        """ Checking id, created and updated  *_at"""
+        creation_inst = BaseModel()
+        self.assertEqual(type(creation_inst.id), str)
+        self.assertEqual(type(creation_inst.created_at), datetime)
+        self.assertEqual(type(creation_inst.updated_at), datetime)
+
