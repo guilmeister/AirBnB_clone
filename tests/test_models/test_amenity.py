3fdacec140f4b944e4a5aa055eebf5184f4d2a67
akeemseymens <812@holbertonschool.com>
2019-11-13 16:21:25 -0800

    added pep8 function

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -9,6 +9,7 @@ import inspect
 from models.amenity import Amenity
 from models.base_model import BaseModel
 import unittest
+import pep8
 
 
 class TestAmenity(unittest.TestCase):
@@ -77,3 +78,35 @@ class TestAmenity(unittest.TestCase):
         string = "[Amenity] ({}) {}".format(self.dummy_amenity.id,
                                             self.dummy_amenity.__dict__)
         self.assertEqual(string, str(self.dummy_amenity))
+
+    import unittest
+from models.place import Place
+import pep8
+
+
+class TestState(unittest.TestCase):
+    """testing inherited functionality from BaseModel"""
+
+    def setUp(self):
+        """test obj instantiation"""
+        self.obj = Place()
+
+    def test_Attrs(self):
+        """checks the existence of expected attrs"""
+        self.assertTrue(hasattr(self.obj, 'city_id'))
+        self.assertTrue(hasattr(self.obj, 'user_id'))
+        self.assertTrue(hasattr(self.obj, 'name'))
+        self.assertTrue(hasattr(self.obj, 'description'))
+        self.assertTrue(hasattr(self.obj, 'number_rooms'))
+        self.assertTrue(hasattr(self.obj, 'number_bathrooms'))
+        self.assertTrue(hasattr(self.obj, 'max_guest'))
+        self.assertTrue(hasattr(self.obj, 'price_by_night'))
+        self.assertTrue(hasattr(self.obj, 'latitude'))
+        self.assertTrue(hasattr(self.obj, 'longitude'))
+        self.assertTrue(hasattr(self.obj, 'amenity_ids'))
+
+    def test_pep8_conformance(self):
+        """test for pep8 conformance"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/amenity.py'])
+        self.assertEqual(result.total_errors, 0, "Found pep8 errors")
